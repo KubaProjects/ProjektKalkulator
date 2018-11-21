99a6a1a10c815bac64d32280ad42345ce8b8ab44
Jakub Barański <01123039@pw.edu.pl>
2018-11-21 12:47:40 +0100

    Added small changes in configuration

--- src/main/java/com/mwo/calculator/CalculatorApplication.java
+++ src/main/java/com/mwo/calculator/CalculatorApplication.java
@@ -2,10 +2,8 @@ package com.mwo.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
 public class CalculatorApplication {
 
     public static void main(String[] args) {
